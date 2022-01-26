2326dba921c17732a62b299fb8f538cb14e55cfa
danhrob <danhrobdev123@gmail.com>
2022-01-26 11:43:27 +0100

    update

--- UserService/Database/Entities/User.cs
+++ UserService/Database/Entities/User.cs
@@ -10,6 +10,6 @@ namespace UserService.Database.Entities
         public int UserId { get; set; }
         public string Name { get; set; }
         public string Adress { get; set; }
-        public string Contact { get; set; }
+        public string Contact { get; set; } 
     }
 }
